7c56ead8e6c551645ae257225a81b6d690bc9131
Lindsey Sandberg <sandbergy@yahoo.com>
2019-02-06 15:10:49 -0600

    add quotes route to server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 let express = require('express');// without ./ automatically goes to node modules
+let PORT = 5000;
 let quotes = require('./quotes');
-let app = express();
 
+let app = express();// set to a variable so that you dont reset application for each .get and .use
+//variable makes it so that when calling .listen(5000) they are still part of the same application and can call on eachother
 
-app.get('/quotes', function (req, res) {
+app.get('/quotes',  (req, res) => {    
+    
     res.send(quotes);
+})
+
+app.listen(PORT, () => {
+    console.log(`Running on ${PORT}`);
 });
-
-
-app.listen(5000, function () {
-    console.log('Running on port 5000');
-
-});
-
