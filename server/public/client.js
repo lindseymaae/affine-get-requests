159577e99c92c12e06a7209a4e43662904f66407
Lindsey Sandberg <sandbergy@yahoo.com>
2019-02-06 16:22:54 -0600

    ajax loaded and working

--- server/public/client.js
+++ server/public/client.js
@@ -2,15 +2,21 @@ $(document).ready(readyNow);
 
 function readyNow() {
     console.log('jQuery has been loaded');
-    addQuote();
-}
 
-function addQuote() {
+    $.ajax({
+        url: '/quotes',
+        method: 'GET'
+    }).then(function (response){
+        console.log('the quotes get request worked', response);
+        
+    });
+
     $('#quotetableBody').append(`   
              <tr>
                 <td>"Remember there's no such thing as a small act of kindness. Every act creates a ripple with no
                     logical end."</td>
                 <td>Scott Adams</td>
             </tr>
-`);
+    `);
+
 }
