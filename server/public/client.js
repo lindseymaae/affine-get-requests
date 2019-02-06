6cc4d081267ebb711eb8ce31833a0071175c7632
Lindsey Sandberg <sandbergy@yahoo.com>
2019-02-06 15:55:14 -0600

    added styles

---
+++ server/public/client.js
@@ -0,0 +1,16 @@
+$(document).ready(readyNow);
+
+function readyNow() {
+    console.log('jQuery has been loaded');
+    addQuote();
+}
+
+function addQuote() {
+    $('#tableBody').append(`   
+             <tr>
+                <td>"Remember there's no such thing as a small act of kindness. Every act creates a ripple with no
+                    logical end."</td>
+                <td>Scott Adams</td>
+            </tr>
+`);
+}
