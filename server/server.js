e2e3fb4c4de52fd03925e10c394189f691b4b592
Lindsey Sandberg <sandbergy@yahoo.com>
2019-02-06 15:26:56 -0600

    created thiings

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ let quotes = require('./quotes');
 
 let app = express();// set to a variable so that you dont reset application for each .get and .use
 //variable makes it so that when calling .listen(5000) they are still part of the same application and can call on eachother
-
+app.use(express.static('server/public'));
 app.get('/quotes',  (req, res) => {    
     
     res.send(quotes);
